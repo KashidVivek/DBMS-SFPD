bd01d5f0f3ebdb2d15ebc98063c7e2575e3e5535
Richard Chou <richard.chou@gmail.com>
2019-11-05 17:13:48 -0500

    1)Working on the data loading process

--- SQL/make_insert/make_insert.py
+++ SQL/make_insert/make_insert.py
@@ -1,5 +1,3 @@
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt # plotting
 import numpy as np # linear algebra
 import os # accessing directory structure
@@ -7,3 +5,12 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 print("hello world")
 
+nRowsRead = 1000 # specify 'None' if want to read whole file
+# Police_Department_Incident_Reports__2018_to_Present.csv has 223958 rows in reality, but we are only loading/previewing the first 1000 rows
+df1 = pd.read_csv('./Police_Department_Incident_Reports__2018_to_Present.csv', delimiter=',', nrows = nRowsRead)
+df1.dataframeName = 'Police_Department_Incident_Reports__2018_to_Present.csv'
+nRow, nCol = df1.shape
+print(f'There are {nRow} rows and {nCol} columns')
+
+
+print(df1.head(5))
