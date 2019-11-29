eadf1869c4753ade06a86da55e987a5556e29ffa
mrrichardchou <richard.chou@gmail.com>
2019-11-29 17:06:56 -0500

    1)Updated SIMPLE.sql

--- SIMPLE.sql
+++ SIMPLE.sql
@@ -29,13 +29,36 @@ ORDER BY
 
 --3.	Find a particular area of city where most of the crime happen.
 
+SELECT
+    COUNT(*) AS cnt,
+    c.analysis_neighborhood AS an
+FROM
+    main               a,
+    incident           b,
+    incident_address   c
+WHERE
+    a.incident_code = b.incident_code
+    AND a.cnn = c.cnn
+    AND c.analysis_neighborhood <> 'null'
+GROUP BY
+    c.analysis_neighborhood
+ORDER BY
+    cnt DESC;
+
 --4.	Determine the number of cases resolved in San Francisco.
-
-
+SELECT
+    COUNT(*)
+FROM
+    main      a,
+    reports   b
+WHERE
+    a.incident_id = b.incident_id
+    AND b.resolution = 'Cite or Arrest Adult';
+    
 --5.	Determine the safest area of the city.
 SELECT
     COUNT(*) AS cnt,
-    c.analysis_neighborhood   AS an
+    c.analysis_neighborhood AS an
 FROM
     main               a,
     incident           b,
@@ -50,12 +73,12 @@ ORDER BY
     cnt ASC;
     
 --6.	Determine the time and a particular area where there is maximum probability of crime scene.
--- not do this, no time
+-- not doing this, no time
 
 --7.	Find if reporting the crime online as soon as crime is committed can help resolve the issues faster.
+-- RC change this to a trend
 
 --8.	Find the most committed crime in every district of San Francisco. 
-
 SELECT
     COUNT(*) AS cnt,
     b.incident_category       AS n,
@@ -75,14 +98,24 @@ ORDER BY
 
 
 --9.	Find the type of crime which is hardest to resolve.
-
+   
 SELECT
-    incident_code
+    COUNT(*) as cnt, 
+    c.incident_category,
+    b.resolution
 FROM
     main      a,
-    reports   b
+    reports   b,
+    incident  c
 WHERE
-    a.incident_number = b.incident_number;
+    a.incident_id = b.incident_id
+    AND b.resolution = 'Open or Active'
+    AND a.incident_code = c.incident_code 
+GROUP  BY
+    c.incident_category, b.resolution
+Order BY
+    cnt DESC
+    ;
 
 --10.	Determine the time period when the crime is least likely to happen.
 -- will not do because we don't have time now
