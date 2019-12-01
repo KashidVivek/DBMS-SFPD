3875186545d7521f1f60d4233b949938d122d775
mrrichardchou <richard.chou@gmail.com>
2019-11-30 22:07:06 -0500

    1)Simple queries are almost working

--- Crime_Visualisation/mainApp/views.py
+++ Crime_Visualisation/mainApp/views.py
@@ -45,7 +45,16 @@ def user_page(request):
     return render(request, 'mainApp/user_page.html')
 
 
-def simple_sql():
+def exec_sql(sql_str):
+    with connection.cursor() as cursor:
+        cursor.execute(sql_str)
+        row = cursor.fetchall()
+    return row
+
+
+def simple(request):
+    result_t = {}
+    # --1.	Determine the day of week when most crime happen.
     sql_str = """
     SELECT
         COUNT(*) AS cnt,
@@ -56,15 +65,150 @@ def simple_sql():
         incident_day_of_week
     ORDER BY
         cnt DESC"""
+    result = exec_sql(sql_str)
+    print(result)
+    result_t['row_id'] = result
+    result_t['r1'] = result
 
-    with connection.cursor() as cursor:
-        cursor.execute(sql_str)
-        row = cursor.fetchall()
-    return row
+    # --2.	Find the most committed crime.
+    sql_str = """
+    SELECT * FROM (
+SELECT
+    COUNT(*) AS cnt,
+    a.incident_code       AS c,
+    b.incident_category   AS n
+FROM
+    main       a,
+    incident   b
+WHERE
+    a.incident_code = b.incident_code
+GROUP BY
+    a.incident_code,
+    b.incident_category
+ORDER BY
+    cnt DESC
+    ) WHERE rownum <= 10
+    """
+    result = exec_sql(sql_str)
+    print(result)
+    result_t['r2'] = result
+
+    # --3.	Find a particular area of city where most of the crime happen.
+    sql_str = """
+SELECT
+    COUNT(*) AS cnt,
+    c.analysis_neighborhood AS an
+FROM
+    main               a,
+    incident           b,
+    incident_address   c
+WHERE
+    a.incident_code = b.incident_code
+    AND a.cnn = c.cnn
+    AND c.analysis_neighborhood <> 'null'
+GROUP BY
+    c.analysis_neighborhood
+ORDER BY
+    cnt DESC"""
+    result = exec_sql(sql_str)
+    print(result)
+    result_t['r3'] = result
+
+    # --4.	Determine the number of cases resolved in San Francisco.
+    sql_str = """
+SELECT
+    COUNT(*)
+FROM
+    main      a,
+    reports   b
+WHERE
+    a.incident_id = b.incident_id
+    AND b.resolution = 'Cite or Arrest Adult'"""
+    result = exec_sql(sql_str)
+    print(result)
+    result_t['r4'] = result
+
+#--5.	Determine the safest area of the city.
+    sql_str = """SELECT * FROM (
+        SELECT
+            COUNT(*) AS cnt,
+            c.analysis_neighborhood AS an
+        FROM
+            main               a,
+            incident           b,
+            incident_address   c
+        WHERE
+            a.incident_code = b.incident_code
+            AND a.cnn = c.cnn
+            AND c.analysis_neighborhood <> 'null'
+        GROUP BY
+            c.analysis_neighborhood
+        ORDER BY
+            cnt ASC
+        ) WHERE ROWNUM <=10
+    """
+    result = exec_sql(sql_str)
+    print(result)
+    result_t['r5'] = result
+
+#--6.	Determine the time and a particular area where there is maximum probability of crime scene.
+#-- not doing this, no time, we could change it to date?
 
 
-def simple(request):
-    row_id = simple_sql()
-    print(row_id)
-    row_id_dict = {'row_id': row_id}
-    return render(request, 'mainApp/simple.html', context=row_id_dict)
+#--7.	Find if reporting the crime online as soon as crime is committed can help resolve the issues faster.
+#-- RC change this to a trend
+
+#--8.	Find the most committed crime in every district of San Francisco.
+    sql_str = """SELECT * FROM (
+        SELECT
+            COUNT(*) AS cnt,
+            b.incident_category       AS n,
+            c.analysis_neighborhood   AS an
+        FROM
+            main               a,
+            incident           b,
+            incident_address   c
+        WHERE
+            a.incident_code = b.incident_code
+            AND a.cnn = c.cnn
+        GROUP BY
+            c.analysis_neighborhood,
+            b.incident_category
+        ORDER BY
+            cnt DESC
+    )WHERE ROWNUM <=10
+    """
+    result = exec_sql(sql_str)
+    print(result)
+    result_t['r8'] = result
+
+#--9.	Find the type of crime which is hardest to resolve.
+    sql_str = """SELECT * FROM (
+        SELECT
+            COUNT(*) as cnt,
+            c.incident_category,
+            b.resolution
+        FROM
+            main      a,
+            reports   b,
+            incident  c
+        WHERE
+            a.incident_id = b.incident_id
+            AND b.resolution = 'Open or Active'
+            AND a.incident_code = c.incident_code
+        GROUP  BY
+            c.incident_category, b.resolution
+        Order BY
+            cnt DESC
+    ) WHERE ROWNUM <=10
+    """
+    result = exec_sql(sql_str)
+    print(result)
+    result_t['r9'] = result
+
+    # --10.	Determine the time period when the crime is least likely to happen.
+    # -- will not do because we don't have time now
+
+    # --11.	Determine the most prevalent crime committed in a certain region.
+    # -- RC this is actually the same as #8 
+    return render(request, 'mainApp/simple.html', context=result_t)
